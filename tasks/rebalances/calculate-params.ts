c018988229a08658f1eb692f2d5f33a848b28349
bweick <brian.weickmann@gmail.com>
2021-12-28 09:59:16 -0800

    Prints Balancer routes for specified asset.

--- tasks/rebalances/calculate-params.ts
+++ tasks/rebalances/calculate-params.ts
@@ -30,11 +30,11 @@ task("calculate-params", "Calculates new rebalance details for an index")
     for (let i = 0; i < assets.length; i++) {
       console.log(assets[i]);
       const quotes: ExchangeQuote[] = [
-        await getSushiswapQuote(deployHelper, info[assets[i]].address, FIFTY_BPS_IN_PERCENT),
-        await getUniswapV2Quote(hre.ethers.provider, info[assets[i]].address, FIFTY_BPS_IN_PERCENT),
-        await getUniswapV3Quote(deployHelper, info[assets[i]].address, FORTY_BPS_IN_PERCENT),
-        await getKyberDMMQuote(hre.ethers.provider, info[assets[i]].address, FIFTY_BPS_IN_PERCENT),
-        await getBalancerV1Quote(hre.ethers.provider, info[assets[i]].address, FIFTY_BPS_IN_PERCENT),
+        await getSushiswapQuote(deployHelper, info["AAVE"].address, FIFTY_BPS_IN_PERCENT),
+        await getUniswapV2Quote(hre.ethers.provider, info["AAVE"].address, FIFTY_BPS_IN_PERCENT),
+        await getUniswapV3Quote(deployHelper, info["AAVE"].address, FORTY_BPS_IN_PERCENT),
+        await getKyberDMMQuote(hre.ethers.provider, info["AAVE"].address, FIFTY_BPS_IN_PERCENT),
+        await getBalancerV1Quote(hre.ethers.provider, info["AAVE"].address, FIFTY_BPS_IN_PERCENT),
       ];
       console.log(quotes.reduce((p, c) => BigNumber.from(p.size).gt(BigNumber.from(c.size)) ? p : c));
     }
